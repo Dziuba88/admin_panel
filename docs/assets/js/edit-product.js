3fbc42aecb273f7efff3531d13d5c58feb62344b
Dziuba88 <Dziuba88@Gmail.com>
2020-09-09 17:45:10 +0300

    .

---
+++ docs/assets/js/edit-product.js
@@ -0,0 +1,193 @@
+var KTBootstrapSwitch = (function () {
+  var states = [
+    "-- NONE --",
+    "Alabama",
+    "Alaska",
+    "Arizona",
+    "Arkansas",
+    "California",
+    "Colorado",
+    "Connecticut",
+    "Delaware",
+    "Florida",
+    "Georgia",
+    "Hawaii",
+    "Idaho",
+    "Illinois",
+    "Indiana",
+    "Iowa",
+    "Kansas",
+    "Kentucky",
+    "Louisiana",
+    "Maine",
+    "Maryland",
+    "Massachusetts",
+    "Michigan",
+    "Minnesota",
+    "Mississippi",
+    "Missouri",
+    "Montana",
+    "Nebraska",
+    "Nevada",
+    "New Hampshire",
+    "New Jersey",
+    "New Mexico",
+    "New York",
+    "North Carolina",
+    "North Dakota",
+    "Ohio",
+    "Oklahoma",
+    "Oregon",
+    "Pennsylvania",
+    "Rhode Island",
+    "South Carolina",
+    "South Dakota",
+    "Tennessee",
+    "Texas",
+    "Utah",
+    "Vermont",
+    "Virginia",
+    "Washington",
+    "West Virginia",
+    "Wisconsin",
+    "Wyoming",
+  ];
+
+  var substringMatcher = function (strs) {
+    return function findMatches(q, cb) {
+      var matches, substringRegex;
+      matches = [];
+      substrRegex = new RegExp(q, "i");
+      $.each(strs, function (i, str) {
+        if (substrRegex.test(str)) {
+          matches.push(str);
+        }
+      });
+      cb(matches);
+    };
+  };
+
+  var demos = function () {
+    var image1 = new KTImageInput("kt_image_1");
+
+    const mediaItem = document.querySelectorAll(".media-item");
+    mediaItem.forEach((imageInput) => {
+      const image = new KTImageInput(imageInput);
+
+      image.on("cancel", function (imageInput) {
+        swal.fire({
+          title: "Image successfully canceled !",
+          type: "success",
+          buttonsStyling: false,
+          confirmButtonText: "Awesome!",
+          confirmButtonClass: "btn btn-primary font-weight-bold",
+        });
+      });
+
+      image.on("change", function (imageInput) {
+        swal.fire({
+          title: "Image successfully changed !",
+          type: "success",
+          buttonsStyling: false,
+          confirmButtonText: "Awesome!",
+          confirmButtonClass: "btn btn-primary font-weight-bold",
+        });
+      });
+
+      image.on("remove", function (imageInput) {
+        swal.fire({
+          title: "Image successfully removed !",
+          type: "error",
+          buttonsStyling: false,
+          confirmButtonText: "Got it!",
+          confirmButtonClass: "btn btn-primary font-weight-bold",
+        });
+      });
+    });
+
+    tinymce.init({
+      selector: "#kt-tinymce-4",
+      menubar: false,
+      toolbar: [
+        "styleselect fontselect fontsizeselect",
+        "undo redo | cut copy paste | bold italic | link image | alignleft aligncenter alignright alignjustify",
+        "bullist numlist | outdent indent | blockquote subscript superscript | advlist | autolink | lists charmap | print preview |  code",
+      ],
+      plugins: "advlist autolink link image lists charmap print preview code",
+    });
+
+    $(".kt-selectpicker").selectpicker();
+    $("[data-switch=true]").bootstrapSwitch();
+
+    $(
+      "#kt_typeahead_1, #kt_typeahead_2, #kt_typeahead_3, .typeahead-input"
+    ).typeahead(
+      { hint: true, highlight: true, minLength: 1 },
+      { name: "states", source: substringMatcher(states) }
+    );
+
+    $("#kt_repeater_1").repeater({
+      initEmpty: false,
+
+      defaultValues: {
+        "text-input": "foo",
+      },
+
+      show: function () {
+        $(this).slideDown();
+      },
+
+      hide: function (deleteElement) {
+        $(this).slideUp(deleteElement);
+      },
+    });
+
+    $("#kt_repeater_2").repeater({
+      initEmpty: false,
+
+      show: function () {
+        $(this).slideDown();
+      },
+
+      hide: function (deleteElement) {
+        $(this).slideUp(deleteElement);
+      },
+    });
+
+    $("#kt_touchspin_1, #kt_touchspin_2").TouchSpin({
+      buttondown_class: "btn btn-light-danger",
+      buttonup_class: "btn btn-light-success",
+      min: 0,
+      max: 1000,
+      step: 0.1,
+      decimals: 2,
+      boostat: 5,
+      maxboostedstep: 10,
+    });
+    $("#kt_touchspin_3, #kt_touchspin_4").TouchSpin({
+      buttondown_class: "btn btn-light-danger",
+      buttonup_class: "btn btn-light-success",
+      min: 0,
+      max: 10000,
+      step: 1,
+      decimals: 0,
+    });
+    $("#kt_select_1").select2({
+      placeholder: "Select categories",
+    });
+    $("#kt_select_2").select2({
+      placeholder: "Select products",
+    });
+  };
+
+  return {
+    // public functions
+    init: function () {
+      demos();
+    },
+  };
+})();
+
+jQuery(document).ready(function () {
+  KTBootstrapSwitch.init();
+});
---
+++ docs/assets/js/faq-categories.js
@@ -0,0 +1,137 @@
+"use strict";
+// Class definition
+
+var KTDatatableDataLocalDemo = (function () {
+  // Private functions
+
+  // demo initializer
+  var demo = function () {
+    const itemsCount = 100;
+
+    function createData(count) {
+      const data = [];
+      for (let i = 0; i < count; i++) {
+        data.push({
+          CategoryID: i,
+          CategoryOrder: count - i,
+          CategoryName: "Category Name " + i,
+          Status: i & 1 ? 1 : 2,
+          Type: i & 1 ? 1 : 2,
+          Actions: null,
+        });
+      }
+
+      return data;
+    }
+
+    var dataJSONArray = createData(itemsCount);
+
+    var datatable = $("#kt_datatable").KTDatatable({
+      // datasource definition
+      data: {
+        type: "local",
+        source: dataJSONArray,
+        pageSize: 10,
+      },
+
+      // layout definition
+      layout: {
+        scroll: false,
+        // enable/disable datatable scroll both horizontal and vertical when needed.
+        // height: 450, // datatable's body's fixed height
+        footer: false,
+        // display/hide footer
+      },
+
+      // column sorting
+      sortable: true,
+
+      pagination: true,
+
+      search: { input: $("#kt_datatable_search_query"), key: "generalSearch" },
+
+      // columns definition
+      columns: [
+        {
+          field: "CategoryID",
+          title: "#",
+          sortable: false,
+          width: 20,
+          type: "number",
+          selector: {
+            class: "",
+          },
+          textAlign: "center",
+        },
+        {
+          field: "CategoryName",
+          title: "Title",
+          autoHide: false,
+        },
+        {
+          field: "CategoryOrder",
+          title: "Order",
+          width: 80,
+          textAlign: "center",
+        },
+        {
+          field: "Status",
+          title: "Status",
+          width: 160,
+          template: function (row) {
+            var status = {
+              1: {
+                title: "Published",
+                class: " label-light-success",
+              },
+              2: {
+                title: "Disabled",
+                class: " label-light-danger",
+              },
+            };
+            return (
+              '<span class="label font-weight-bold label-lg ' +
+              status[row.Status].class +
+              ' label-inline">' +
+              status[row.Status].title +
+              "</span>"
+            );
+          },
+        },
+        {
+          field: "Actions",
+          title: "Actions",
+          sortable: false,
+          width: 80,
+          overflow: "visible",
+          autoHide: false,
+          template: function () {
+            return '<a href="edit-category.html" class="btn btn-sm btn-clean btn-outline-primary btn-icon mr-2" data-toggle="tooltip" data-placement="left" title="Edit details"><i class="la la-edit"></i></a><a href="javascript:;" class="btn btn-sm btn-outline-danger btn-icon" data-toggle="tooltip" data-placement="left" title="Delete item"><i class="la la-trash-alt"></i></a>';
+          },
+        },
+      ],
+    });
+
+    $("#kt_datatable_search_status").on("change", function () {
+      datatable.search($(this).val().toLowerCase(), "Status");
+    });
+
+    $("#kt_datatable_search_type").on("change", function () {
+      datatable.search($(this).val().toLowerCase(), "Type");
+    });
+
+    $("#kt_datatable_search_status, #kt_datatable_search_type").selectpicker();
+  };
+
+  return {
+    // Public functions
+    init: function () {
+      // init dmeo
+      demo();
+    },
+  };
+})();
+
+jQuery(document).ready(function () {
+  KTDatatableDataLocalDemo.init();
+});
